94bef03b544f63a701df957814aaa61770593881
root <root@DESKTOP-A9R1SCS.>
2024-10-18 23:49:35 +0800

    upgrade

--- exercises/tests/tests8.rs
+++ exercises/tests/tests8.rs
@@ -7,7 +7,6 @@
 // Execute `rustlings hint tests8` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn main() {}
 
@@ -17,8 +16,9 @@ mod tests {
 
     #[test]
     fn test_success() {
-        #[cfg(feature = "pass")]
-        return;
+        if cfg!(feature = "pass") {
+            return;
+        }
 
         panic!("no cfg set");
     }
